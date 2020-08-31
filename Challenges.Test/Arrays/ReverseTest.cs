64af52a67cc86539dd2481b287aa880777ae9296
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-08-31 14:46:22 -0500

    tests for reverse

--- Challenges.Test/Arrays/ReverseTest.cs
+++ Challenges.Test/Arrays/ReverseTest.cs
@@ -1,13 +1,21 @@
-﻿using Xunit;
+﻿using Challenges401.Arrays;
+using Xunit;
 
 namespace Challenges.Test
 {
     public class ReverseTest
     {
         [Fact]
-        public void Test1()
+        public void can_Reverse_Array()
         {
+            //Arrange
+            int[] testArray = new[] { 1, 2, 3 };
 
+            //Act
+            int[] result = Reverse.ReverseArray(testArray);
+
+            //Assert
+            Assert.Equal(new[] { 3, 2, 1 }, result);
         }
     }
 }
