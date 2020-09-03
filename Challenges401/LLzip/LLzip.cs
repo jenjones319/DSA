efc49dcb07ca303d10db252009e7acc568f98d38
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-03 00:16:29 -0500

    llzip challenge

---
+++ Challenges401/LLzip/LLzip.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures.LinkedLists;
+
+namespace Challenges401.LLzip
+{
+    public class LLZip
+    {
+        public static LinkedList Zip(LinkedList listA, LinkedList listB)
+        {
+            Node currentA = listA.Head;
+            Node currentB = listB.Head;
+
+            LinkedList result = new LinkedList();
+
+            while (currentA != null || currentB != null)
+            {
+                if (currentA != null)
+                {
+                    result.Append(currentA.Value);
+                    currentA = currentA.Next;
+                }
+                if (currentB != null)
+                {
+                    result.Append(currentB.Value);
+                    currentB = currentB.Next;
+                }
+            }
+            return result;
+        }
+    }
+
+}
