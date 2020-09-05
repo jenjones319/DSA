6030bf6acd79f1e29933faa89798c2a2132ceaf4
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-05 10:32:35 -0500

    added search challenge

---
+++ Challenges.Test/LLzip/LLzipTest.cs
@@ -0,0 +1,63 @@
+﻿using Challenges401.LLzip;
+using DataStructures.LinkedLists;
+using Xunit;
+
+namespace Challenges.Test.LLzipTest
+{
+    public class LLzipTest
+    {
+        [Fact]
+        public void LinkedList_zip_lists_together()
+        {
+            LinkedList listA = new LinkedList();
+            LinkedList listB = new LinkedList();
+
+            listA.Append(1);
+            listA.Append(3);
+            listA.Append(5);
+
+            listB.Append(2);
+            listB.Append(4);
+            listB.Append(6);
+
+            LinkedList expected = new LinkedList();
+
+            expected.Append(1);
+            expected.Append(2);
+            expected.Append(3);
+            expected.Append(4);
+            expected.Append(5);
+            expected.Append(6);
+
+            LinkedList result = LLZip.Zip(listA, listB);
+
+            Assert.Equal(expected.ToString(), result.ToString());
+        }
+
+        [Fact]
+        public void LinkedList_zip_two_different_sized_list()
+        {
+            LinkedList listA = new LinkedList();
+            LinkedList listB = new LinkedList();
+
+
+            listA.Append(1);
+
+            listB.Append(2);
+            listB.Append(3);
+            listB.Append(4);
+
+            LinkedList expected = new LinkedList();
+
+
+            expected.Append(1);
+            expected.Append(2);
+            expected.Append(3);
+            expected.Append(4);
+
+            LinkedList result = LLZip.Zip(listA, listB);
+
+            Assert.Equal(expected.ToString(), result.ToString());
+        }
+    }
+}
