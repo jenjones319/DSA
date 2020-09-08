3a877552aa29c401674ae1c96aed9a4c31e05854
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-07 22:03:36 -0500

    added search and search.test

--- Challenges401/Arrays/Search.cs
+++ Challenges401/Arrays/Search.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Challenges401.Arrays
+﻿namespace Challenges401.Arrays
 {
     class Search
     {
+        public static int BiSearch(int value, int[] array)
+        {
+            int left = 0;
+            int right = array.Length;
+            int mid = array.Length / 2;
+
+            while (value != array[mid])
+            {
+                if (value > array[mid])
+                {
+                    left = mid;
+                }
+                else if (value < array[mid])
+                {
+                    right = mid;
+                }
+                mid = ((right - left) / 2) + 2;
+                if ((left == mid || right == mid) && array[mid] != value)
+                {
+                    return -1;
+                }
+            }
+
+            return mid;
+        }
     }
 }
